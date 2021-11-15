67f73d36cc5ae6b4c48379360bbf7319932d5779
SW_Creeperking <sw.creeperking@gmail.com>
2021-11-15 01:54:01 -0800

    Added Cooldown.cs

---
+++ RayWrapper/Vars/Cooldown.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static RayWrapper.GameBox;
+
+namespace RayWrapper.Vars
+{
+    public class Cooldown
+    {
+        public long durationMs;
+        public long LastTick { get; private set; }
+
+        public Cooldown(long durationMs, bool startWithCooldown = false) =>
+            (this.durationMs, LastTick) = (durationMs, GetTimeMs() - (startWithCooldown ? 0 : durationMs));
+
+        public bool UpdateTime()
+        {
+            var time = GetTimeMs();
+            if (LastTick + durationMs < time) return false;
+            LastTick = time;
+            return true;
+        }
+
+        public float GetRemainingTime() => Math.Max(durationMs - GetTimeMs() - LastTick, 0);
+        public float GetTimePercent() => (float)(GetTimeMs() - LastTick) / durationMs;
+    }
+}
