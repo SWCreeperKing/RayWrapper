7e16216a1694d504ca42c5e2b83210be4fe5ae81
SW_Creeperking <sw.creeperking@gmail.com>
2021-12-13 18:36:35 -0800

    0.1.15

---
+++ RayWrapper/Vars/GhostObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace RayWrapper.Vars
+{
+    public abstract class GhostObject : GameObject
+    {
+        public override Vector2 Position => Vector2.Zero;
+        public override Vector2 Size => Vector2.Zero;
+    }
+}
