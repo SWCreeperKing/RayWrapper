a23f0e35735e0c16805e7e93a1ad00e6dd71e046
ZimonIsHim <dutchsimon9@gmail.com>
2022-02-15 22:35:46 +0100

    Created INodeShape interface

---
+++ RayWrapper/Objs/TreeView/TreeNodeChain/NodeShapes/INodeShape.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace RayWrapper.Objs.TreeView.TreeNodeChain.NodeShapes
+{
+    public interface INodeShape
+    {
+        public bool IsMouseIn(Vector2 off, float scale);
+
+        public void DrawOnHover(Vector2 off, float scale);
+        public Vector2 Center(Vector2 off, float scale);
+        public abstract void DrawShape(Vector2 off, float scale);
+
+        public void Update(Vector2 off, float scale, bool context, bool next);
+
+        public void Draw(Vector2 off, float scale);
+    }
+}
