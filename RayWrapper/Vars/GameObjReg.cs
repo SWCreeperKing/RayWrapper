97aaf31d3a93697a1f8928f177bbbf9b284c6437
ZimonIsHim <dutchsimon9@gmail.com>
2022-02-15 22:44:13 +0100

    Created IGameObjReg interface

--- RayWrapper/Vars/GameObjReg.cs
+++ RayWrapper/Vars/GameObjReg.cs
@@ -2,10 +2,11 @@
 
 namespace RayWrapper.Vars
 {
-    public abstract class GameObjReg
+    public abstract class GameObjReg : IGameObjReg
     {
+        // TODO: Look into the GameObject & GameObjReg system. This should not use the GameObject class since GameObjReg is the superclass of that class.
         private List<GameObject> _registry = new();
-        
+
         public void UpdateReg()
         {
             foreach (var a in _registry) a.Update();
@@ -15,7 +16,7 @@ namespace RayWrapper.Vars
         {
             foreach (var a in _registry) a.Render();
         }
-        
+
         public void RegisterGameObj(params GameObject[] igo) => _registry.AddRange(igo);
         public void DeregisterGameObj(GameObject igo) => _registry.Remove(igo);
         public GameObject[] GetRegistry() => _registry.ToArray();
---
+++ RayWrapper/Vars/IGameObjReg.cs
@@ -0,0 +1,11 @@
+﻿namespace RayWrapper.Vars
+{
+    public interface IGameObjReg
+    {
+        public void UpdateReg();
+        public void RenderReg();
+        public void RegisterGameObj(params GameObject[] igo);
+        public void DeregisterGameObj(GameObject igo);
+        public GameObject[] GetRegistry();
+    }
+}
