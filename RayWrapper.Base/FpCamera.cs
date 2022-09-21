2e5d605cade6f84c401c313a2f14c6c036acec1c
SW_Creeperking <sw.creeperking@gmail.com>
2022-09-21 12:16:00 -0700

    FpCamera port

--- RayWrapper.Base/FpCamera.cs
+++ RayWrapper.Base/FpCamera.cs
@@ -1,6 +1,256 @@
+using System.Numerics;
+using Raylib_CsLo;
+using static Raylib_CsLo.KeyboardKey;
+using static Raylib_CsLo.Raylib;
+using static Raylib_CsLo.RlGl;
+using static RayWrapper.Base.FpCamera.CameraControls;
+
 namespace RayWrapper.Base;
 
+//https://github.com/NotNotTech/Raylib-Extras-CsLo/blob/main/raylibExtras-cs/cameras/rlFPCamera/rlFPCamera.cs
 public class FpCamera
 {
-    
+    private const float Pi180 = MathF.PI / 180f;
+    private const float ViewBobbleDampen = 8f;
+
+    public enum CameraControls
+    {
+        MoveFront = 0,
+        MoveBack,
+        MoveRight,
+        MoveLeft,
+        MoveUp,
+        MoveDown,
+        TurnLeft,
+        TurnRight,
+        TurnUp,
+        TurnDown,
+        Sprint,
+    }
+
+    public Camera3D Camera => _camera;
+
+    public Dictionary<CameraControls, KeyboardKey> keybindings = new()
+    {
+        { MoveFront, KEY_W }, { MoveBack, KEY_S }, { MoveLeft, KEY_A }, { MoveRight, KEY_D }, { MoveUp, KEY_SPACE },
+        { MoveDown, KEY_LEFT_SHIFT }, { TurnDown, KEY_DOWN }, { TurnLeft, KEY_LEFT }, { TurnRight, KEY_RIGHT },
+        { TurnUp, KEY_UP }, { Sprint, KEY_LEFT_CONTROL }
+    };
+
+    public Vector3 moveSpeed = Vector3.One;
+    public Vector2 turnSpeed = new(90);
+    public float mouseSensitivity = 600;
+
+    public float minViewY = -65f;
+    public float maxViewY = 89f;
+    public float viewBobbleFreq = 0f;
+    public float viewBobbleMag = .02f;
+
+    public float viewBobbleWaveMag = .002f;
+    public bool useMouseX = true;
+    public bool useMouseY = true;
+    public bool useKeyboard = true;
+    public bool useController = true;
+    public bool controllerId = false;
+    public bool hideCursor = true;
+
+    /// <summary>clipping plane: must use BeginMode3D and EndMode3D on the camera object for clipping planes to work</summary>
+    public double nearPlane = .01f;
+
+    /// <summary>clipping plane: must use BeginMode3D and EndMode3D on the camera object for clipping planes to work</summary>
+    public double farPlane = 1e3;
+
+    protected Vector3 position = Vector3.Zero;
+    protected Vector2 fov = Vector2.Zero;
+    protected Vector2 prevMousePos = Vector2.Zero;
+    protected Vector2 angle = Vector2.Zero; // Camera angle in plane XZ
+    protected float targetDist = 0; // Camera distance from position to target
+    protected float playerEyesPos = 0.5f; // Player eyes position from ground (in meters)
+    protected float currentBobble;
+    protected bool focused = true;
+
+    private Camera3D _camera = new();
+
+    public PositionCallback? validateCamPos = null;
+
+    public delegate bool PositionCallback(FpCamera camera, Vector3 newPosition, Vector3 oldPosition);
+
+    public FpCamera() => prevMousePos = GetMousePosition();
+
+    public void Setup(float fovY, Vector3 pos)
+    {
+        _camera.position = position = new Vector3(pos.X, pos.Y, pos.Z);
+        _camera.position.Y += playerEyesPos;
+        _camera.target = _camera.position + new Vector3(0, 0, 1);
+        _camera.up = new Vector3(0, 1, 0);
+        _camera.fovy = fovY;
+        _camera.projection_ = CameraProjection.CAMERA_PERSPECTIVE;
+        focused = IsWindowFocused();
+
+        if (hideCursor && focused && (useMouseX || useMouseY)) DisableCursor();
+        targetDist = 1;
+        ViewResized();
+    }
+
+    public void ViewResized()
+    {
+        var height = (float) GetScreenHeight();
+        fov.Y = _camera.fovy;
+        if (height != 0) fov.X = fov.Y * (GetScreenWidth() / height);
+    }
+
+    protected float GetSpeedForAxis(CameraControls axis, float speed)
+    {
+        if (!useKeyboard || !keybindings.ContainsKey(axis)) return 0;
+
+        var key = keybindings[axis];
+        if (key == KEY_NULL) return 0;
+
+        var factor = 1f;
+        if (IsKeyDown(keybindings[Sprint])) factor = 2;
+        if (IsKeyDown(key)) return speed * GetFrameTime() * factor;
+
+        return 0f;
+    }
+
+    public void Update()
+    {
+        if (hideCursor && IsWindowFocused() != focused && (useMouseX || useMouseY))
+        {
+            focused = IsWindowFocused();
+            if (focused) DisableCursor();
+            else EnableCursor();
+        }
+
+        var curMousePos = GetMousePosition();
+        var mousePositionDelta = curMousePos - prevMousePos;
+        prevMousePos = curMousePos;
+
+        // Mouse movement detection
+        var mouseWheelMove = GetMouseWheelMove();
+
+        // Keys input detection
+        var directions = new Dictionary<CameraControls, float>
+        {
+            { MoveFront, GetSpeedForAxis(MoveFront, moveSpeed.Z) },
+            { MoveBack, GetSpeedForAxis(MoveBack, moveSpeed.Z) },
+            { MoveRight, GetSpeedForAxis(MoveRight, moveSpeed.X) },
+            { MoveLeft, GetSpeedForAxis(MoveLeft, moveSpeed.X) }, { MoveUp, GetSpeedForAxis(MoveUp, moveSpeed.Y) },
+            { MoveDown, GetSpeedForAxis(MoveDown, moveSpeed.Y) },
+        };
+
+        var forward = _camera.target - _camera.position;
+        forward.Y = 0;
+        forward = Vector3.Normalize(forward);
+
+        var right = new Vector3(forward.Z * -1.0f, 0, forward.X);
+        var oldPosition = position;
+
+        position += (forward * (directions[MoveFront] - directions[MoveBack]));
+        position += (right * (directions[MoveRight] - directions[MoveLeft]));
+        position.Y += directions[MoveUp] - directions[MoveDown];
+
+        // let someone modify the projected position
+        validateCamPos?.Invoke(this, position, oldPosition);
+
+        // Camera orientation calculation
+        var turnRotation = GetSpeedForAxis(TurnRight, turnSpeed.X) - GetSpeedForAxis(TurnLeft, turnSpeed.X);
+        var tiltRotation = GetSpeedForAxis(TurnUp, turnSpeed.Y) - GetSpeedForAxis(TurnDown, turnSpeed.Y);
+
+        if (turnRotation != 0) angle.X -= turnRotation * Pi180;
+        else if (useMouseX && focused) angle.X += mousePositionDelta.X / -mouseSensitivity;
+
+        if (tiltRotation != 0) angle.Y += tiltRotation * Pi180;
+        else if (useMouseY && focused) angle.Y += mousePositionDelta.Y / -mouseSensitivity;
+
+        // Angle clamp
+        if (angle.Y < minViewY * Pi180) angle.Y = minViewY * Pi180;
+        else if (angle.Y > maxViewY * Pi180) angle.Y = maxViewY * Pi180;
+
+        // Recalculate camera target considering translation and rotation
+        var target = RayMath.Vector3Transform(new Vector3(0, 0, 1),
+            RayMath.MatrixRotateXYZ(new Vector3(angle.Y, -angle.X, 0)));
+
+        _camera.position = position;
+        var eyeOffset = playerEyesPos;
+
+        if (viewBobbleFreq > 0)
+        {
+            var swingDelta = MathF.Max(MathF.Abs(directions[MoveFront] - directions[MoveBack]),
+                MathF.Abs(directions[MoveRight] - directions[MoveLeft]));
+
+            // If movement detected (some key pressed), increase swinging
+            currentBobble += swingDelta * viewBobbleFreq;
+
+            eyeOffset -= MathF.Sin(currentBobble / ViewBobbleDampen) * viewBobbleMag;
+            _camera.up.Z = -(_camera.up.X = MathF.Sin(currentBobble / (ViewBobbleDampen * 2)) * currentBobble);
+        }
+        else currentBobble = _camera.up.X = _camera.up.Z = 0;
+
+        _camera.position.Y += eyeOffset;
+        _camera.target = _camera.position + target;
+    }
+
+    public float GetFovX() => fov.X;
+    public Vector3 GetCameraPosition() => position;
+
+    public void SetCameraPosition(Vector3 pos)
+    {
+        position = pos;
+        var forward = _camera.target - _camera.position;
+        _camera.target = (_camera.position = position) + forward;
+    }
+
+    public Vector2 GetViewAngles() => RayMath.Vector2Scale(angle, Pi180);
+
+    // start drawing using the camera, with near/far plane support
+    public void BeginMode3D()
+    {
+        //Raylib.BeginMode3D(Camera);
+        var aspect = GetScreenWidth() / (float) GetScreenHeight();
+
+        rlDrawRenderBatchActive(); // Draw Buffers (Only OpenGL 3+ and ES2)
+        rlMatrixMode(RL_PROJECTION); // Switch to projection matrix
+        rlPushMatrix(); // Save previous matrix, which contains the settings for the 2d ortho projection
+        rlLoadIdentity(); // Reset current matrix (projection)
+
+        switch (_camera.projection_)
+        {
+            case CameraProjection.CAMERA_PERSPECTIVE:
+            {
+                // Setup perspective projection
+                var top = RL_CULL_DISTANCE_NEAR * MathF.Tan(_camera.fovy * .5f * Pi180);
+                var right = top * aspect;
+
+                rlFrustum(-right, right, -top, top, nearPlane, farPlane);
+                break;
+            }
+            case CameraProjection.CAMERA_ORTHOGRAPHIC:
+            {
+                // Setup orthographic projection
+                var top = _camera.fovy / 2f;
+                var right = top * aspect;
+
+                rlOrtho(-right, right, -top, top, nearPlane, farPlane);
+                break;
+            }
+        }
+
+        // NOTE: zNear and zFar values are important when computing depth buffer values
+
+        rlMatrixMode(RL_MODELVIEW); // Switch back to modelview matrix
+        rlLoadIdentity(); // Reset current matrix (modelview)
+
+        // Setup Camera view
+        var matView = RayMath.MatrixLookAt(_camera.position, _camera.target, _camera.up);
+
+        // TODO, fix this!
+        //float[] mat = rlMathUtils.MatrixToBuffer(matView);
+        rlMultMatrixf(matView); // Multiply modelview matrix by view matrix (camera)
+
+        rlEnableDepthTest(); // Enable DEPTH_TEST for 3D
+    }
+
+    // end drawing with the camera
+    public void EndMode3D() => Raylib.EndMode3D();
 }
---
+++ RayWrapper.Base/Rectangle.cs
@@ -0,0 +1,164 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+using static Raylib_CsLo.Raylib;
+using Rect = Raylib_CsLo.Rectangle;
+
+namespace RayWrapper.Base;
+
+public class Rectangle
+{
+    public static readonly Rect Zero = new(0, 0, 0, 0);
+    public static readonly Rect Max = new(float.MinValue, float.MinValue, float.MaxValue, float.MaxValue);
+
+    public Rect Rect
+    {
+        get => _rect;
+        set
+        {
+            _rect = value;
+            _pos = _pos with { X = _rect.x, Y = _rect.y };
+            _size = _size with { X = _rect.width, Y = _rect.height };
+        }
+    }
+
+    public Vector2 Pos
+    {
+        get => _pos;
+        set
+        {
+            _rect = _rect with { X = value.X, Y = value.Y };
+            _pos = value;
+        }
+    }
+
+    public Vector2 Size
+    {
+        get => _size;
+        set
+        {
+            _rect = _rect with { width = value.X, height = value.Y };
+            _size = value;
+        }
+    }
+
+    public float X
+    {
+        get => _rect.x;
+        set => _rect.x = _pos.X = value;
+    }
+
+    public float Y
+    {
+        get => _rect.y;
+        set => _rect.y = _pos.Y = value;
+    }
+
+    public float W
+    {
+        get => _rect.width;
+        set => _rect.width = _size.X = value;
+    }
+
+    public float H
+    {
+        get => _rect.height;
+        set => _rect.height = _size.Y = value;
+    }
+
+    private Rect _rect;
+    private Vector2 _pos;
+    private Vector2 _size;
+
+    public Rectangle(Rect rect) => Rect = rect;
+    public Rectangle(Vector2 pos, Vector2 size) => Rect = new Rect(pos.X, pos.Y, size.X, size.Y);
+    public Rectangle(float x, float y, float w, float h) => Rect = new Rect(x, y, w, h);
+
+    public void MaskDraw(Action draw) => Pos.MaskDraw(Size, draw);
+    public bool IsV2In(Vector2 v2) => CheckCollisionPointRec(v2, Rect);
+    public bool IsRectIn(Rect rect) => CheckCollisionRecs(Rect, rect);
+    public bool IsCircleIn(Circle c) => c.CheckCollision(Rect);
+    public Vector2 Center() => Pos + Size / 2f;
+    public Rectangle Clone() => new(Rect);
+
+    /// <summary>shrinks all 4 sides by amount</summary>
+    public void Shrink(float amount)
+    {
+        X += amount;
+        Y += amount;
+        W -= amount * 2;
+        H -= amount * 2;
+    }
+
+    /// <summary>Grows all 4 sides by amount</summary>
+    public void Grow(float amount)
+    {
+        X -= amount;
+        Y -= amount;
+        W += amount * 2;
+        H += amount * 2;
+    }
+
+    /// <summary>Changes the position while keeping the position of the size the same</summary>
+    public void ExtendPos(float amount)
+    {
+        X -= amount;
+        Y -= amount;
+        W += amount;
+        H += amount;
+    }
+
+    public void Deconstruct(out float x, out float y, out float w, out float h)
+    {
+        x = X;
+        y = Y;
+        w = W;
+        h = H;
+    }
+
+    public void Draw(Color? color = null) => DrawRectangleRec(Rect, color ?? WHITE);
+
+    public void DrawPro(Vector2? origin = null, float rotation = 0f, Color? color = null)
+    {
+        DrawRectanglePro(Rect, origin ?? Vector2.Zero, rotation, color ?? WHITE);
+    }
+
+    public void DrawLines(float thickness = 3f, Color? color = null)
+    {
+        DrawRectangleLinesEx(Rect, thickness, color ?? WHITE);
+    }
+
+    public void DrawRounded(int segments = 10, float roundness = 1f, Color? color = null)
+    {
+        DrawRectangleRounded(Rect, roundness, segments, color ?? WHITE);
+    }
+
+    public void DrawRoundedLines(int segments = 10, float roundness = 1f, float thickness = 3f, Color? color = null)
+    {
+        DrawRectangleRoundedLines(Rect, roundness, segments, thickness, color ?? WHITE);
+    }
+
+    public void DrawGradient(Color c1, Color c2, Color c3, Color c4)
+    {
+        DrawRectangleGradientEx(Rect, c1, c2, c3, c4);
+    }
+
+    public void DrawGradientV(Color c1, Color c2)
+    {
+        DrawRectangleGradientV((int) X, (int) Y, (int) W, (int) H, c1, c2);
+    }
+
+    public void DrawGradientH(Color c1, Color c2)
+    {
+        DrawRectangleGradientH((int) X, (int) Y, (int) W, (int) H, c1, c2);
+    }
+
+    public static bool operator ==(Rectangle r1, Rect r2) =>
+        r1.X == r2.x && r1.Y == r2.y && r1.W == r2.width && r1.H == r2.height;
+
+    public static bool operator !=(Rectangle r1, Rect r2) => !(r1 == r2);
+
+    public static explicit operator Rectangle(Rect rect) => new(rect);
+    public static implicit operator Rect(Rectangle rect) => rect.Rect;
+
+    public override string ToString() => $"[({X},{Y})({W}x{H})]";
+}
